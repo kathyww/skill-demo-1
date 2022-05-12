b3c59eed9a216cdb702b71d8b755277ded074b77
kathyww <kes.wen404@gmail.com>
2022-05-11 23:42:40 -0700

    update

--- SkillDemo1.java
+++ SkillDemo1.java
@@ -1,3 +1,6 @@
 public class SkillDemo1 {
+    public static int minus(int a, int b) {
+        return a-b;
+    }
 
 }
---
+++ SkillDemo1Tester.java
@@ -0,0 +1,10 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class SkillDemo1Tester {
+    @Test
+    public void minusTester() {
+        assertEquals(3, SkillDemo1.minus(5, 4));
+    }
+    
+}
